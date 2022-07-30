78bba8602078a93229f3e7e81ffeb1311cbcd11b
saif hossain jibon <saifhossainjibon@saifs-MacBook-Air.local>
2022-07-30 16:14:06 -0700

    server start

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Summer Holidays server')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
